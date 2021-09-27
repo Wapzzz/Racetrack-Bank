160bedba529f0594b798c01548e62729fc333eb9
Wapzzz <91371533+Wapzzz@users.noreply.github.com>
2021-09-27 11:04:54 +0000

    Update script.js

--- script.js
+++ script.js
@@ -8,38 +8,70 @@ let host = 'HOST'; // Your machine ip
 let sCode; // Leave that empty
 
 let cookies; // Leave that blank
-let sdata = `user=${user}&amount=1`; // Send-Gold data
 
 let gold = 10100; // How much gold to add
 
 for(let i=1; i<gold; i++) {
-    let rdata = `username=tedd${i}&password=${pass}&password2=${pass}`; // Register data
-    let ldata = `username=tedd${i}&password=${pass}`; // Login data
-    setTimeout(() => {
-    request.post({
-        headers: {'content-type' : 'application/x-www-form-urlencoded'},
-        url: `http://${host}/api/create`,
-        body: rdata
-    }, (err, response, body) => {
+
+    let rdata = `username=teddd${i}&password=${pass}&password2=${pass}`; // Register data
+    let ldata = `username=teddd${i}&password=${pass}`; // Login data
+    let sdata = `user=${user}&amount=1`; // Send-Gold data
+
+    if(i<2000) {
+        setTimeout(() => {
             request.post({
                 headers: {'content-type' : 'application/x-www-form-urlencoded'},
-                url: `http://${host}/api/login`,
-                body: ldata
-            }, (err, res, body) => {      
-                if(!res) return;
-                cookies = res.headers['set-cookie'];
-                if(cookies === undefined) return;
+                url: `http://${host}/api/create`,
+                body: rdata
+            }, (err, response, body) => {
                 request.post({
-                    headers: {
-                        'content-type': 'application/x-www-form-urlencoded',
-                        'Cookie': `${cookies}`,
-                    },
-                url: `http://${host}/api/givegold`,
-                body: sdata,
-                }, (err, resp, body) => {
-                    if(body) console.log(i);
+                    headers: {'content-type' : 'application/x-www-form-urlencoded'},
+                    url: `http://${host}/api/login`,
+                    body: ldata
+                }, (err, res, body) => {      
+                    if(!res) return;
+                    cookies = res.headers['set-cookie'];
+                    if(cookies === undefined) return;
+                    request.post({
+                        headers: {
+                            'content-type': 'application/x-www-form-urlencoded',
+                            'Cookie': `${cookies}`,
+                        },
+                    url: `http://${host}/api/givegold`,
+                    body: sdata,
+                    }, (err, resp, body) => {
+                        if(body) console.log(i);
+                    });
                 });
             });
-        });
-    }, i * 200);
+        }, i * 200);
+    } else {
+        setTimeout(() => {
+            request.post({
+                headers: {'content-type' : 'application/x-www-form-urlencoded'},
+                url: `http://${host}/api/create`,
+                body: rdata
+            }, (err, response, body) => {
+                request.post({
+                    headers: {'content-type' : 'application/x-www-form-urlencoded'},
+                    url: `http://${host}/api/login`,
+                    body: ldata
+                }, (err, res, body) => {      
+                    if(!res) return;
+                    cookies = res.headers['set-cookie'];
+                    if(cookies === undefined) return;
+                    request.post({
+                        headers: {
+                            'content-type': 'application/x-www-form-urlencoded',
+                            'Cookie': `${cookies}`,
+                        },
+                    url: `http://${host}/api/givegold`,
+                    body: sdata,
+                    }, (err, resp, body) => {
+                        if(body) console.log(i);
+                    });
+                });
+            });
+        }, i * 200);
+    };
 };
